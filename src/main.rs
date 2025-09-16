3a2df3099779f1123ab43f6da895fabfecbfad66
He2Henrique <134214073+He2Henrique@users.noreply.github.com>
2025-09-15 21:14:29 -0300

    testing references.

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ use std::io;
 fn main() {
     println!("Hello, world!");
 
+
     let condition = true;
     let number = if condition 
         { println!("u get a sum!"); 
             sum(2, 2)} else { 6 };
 
-
     println!("{number}");
-
 
     loop {
         let mut resposta = String::new();
@@ -26,9 +25,28 @@ fn main() {
         }
     }
 
+    //in rust to u pass references that is references to just read or mutable references
+
+    let mut str = String::from("hello");
+
+    pushing_world(&mut str); 
+
+    let num: usize = takes_len(&str);
+
+    print!("{num}");
+
     
 }
 
 fn sum(x: i32, y: i32) -> i32 {
     x + y
+}
+
+fn pushing_world(s: &mut String){
+    s.push_str("World");
+
+}
+
+fn takes_len(s: &String)-> usize{
+    return s.len(); 
 }
